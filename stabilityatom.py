db1e7a5fc39759bad34156fb71c68df30b94914a
Brankan <60178858+Brankan@users.noreply.github.com>
2022-10-22 11:52:23 +0200

    Update stabilityatom.py

--- stabilityatom.py
+++ stabilityatom.py
@@ -10,15 +10,15 @@ x = 34
 if (A - P) % 2 == 0 and P % 2 == 0:
     onlyo = +34
 else:
-    if A% 2 != 0:
+    if A % 2 != 0:
         onlyo = 0
     else:
         onlyo = -34
 
 Ec = a * A - b * (A ** (2 / 3)) - y * ((P ** 2) / (A ** (1 / 3))) - e * (((A - 2 * P) ** 2) / A) + onlyo * A ** (-3 / 4)
-R = 1.3*A**(1/3)
+R = 1.3 * A ** (1 / 3)
 
-print("Z: " , A / (1.98 + 0.015 * A **(2 / 3)))
-print(Ec , Ec / A)
+print("Z: ", A / (1.98 + 0.015 * A ** (2 / 3)))
+print(Ec, Ec / A)
 print(str(R) + "* 10**-13")
-print("Probability of Spontaneous Fission: " , P ** 2 / A , " , 45 ≈ 100% ")
+print("Probability of Spontaneous Fission: ", P ** 2 / A, " , 45 ≈ 100% ")
